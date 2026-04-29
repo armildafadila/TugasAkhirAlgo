e2ec6e9fb253d5ae307af68e20767a3228d7d154
haidarmdd <124250029@student.upnyk.ac.id>
2026-04-29 17:45:21 +0700

    struct & daftar void

--- rental.cpp
+++ rental.cpp
@@ -3,11 +3,34 @@
 #include <cstring>
 using namespace std;
 
-int main () {
-    struct rental
-    {
-        
-    };
-    
-    
-}
+struct motor
+{
+    char namaMtr[50];
+    char plat[10];
+    char status[20];
+    char penyewa[60];
+    int harga, tahun, nama, total;
+    motor *next;
+};
+
+motor *head = NULL;
+
+// LOADING FILE
+
+// SIMPAN FILE
+
+// TAMBAH DATA MOTOR
+
+// HAPUS DATA MOTOR
+
+// SEWA MOTOR
+
+// KEMBALI MOTOR
+
+// CARI BERDASARKAN NAMA
+// CARI BERDASARKAN PLAT NOMOR
+
+// URUTKAN HARGA (asc)
+// URUTKAN NAMA (A-Z)
+
+// TAMPIL SEUA DATA
